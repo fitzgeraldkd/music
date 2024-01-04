1f364685bab3bf6222f9e42e654dfd3397d100d2
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2024-01-03 20:19:38 -0500

    Fix CORS issue with visualizer

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Audio Files
 
-The audio files are not included in this repository. When running locally, they will need to be saved to the `./public/music/` directory, using paths consistent with the `AUDIO_FILE_PATHS` constant. The files are served from AWS S3 for the production environment.
+The audio files are served from AWS S3. When releasing new songs, the files either need to be added to a folder that has public read access granted through the bucket policy, or a new statement needs to be added to grant permission to the folder.
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,9 +6,4 @@ export const AUDIO_FILE_PATHS = {
 
 // The files in this bucket are not sensitive, and there is a bucket policy to only allow access to files/folders
 // that should be publicly accessible (i.e. prevent users from accessing a song before its release date).
-// For development, read from the local public directory to allow the audio player and analyzer to work properly.
-export const BUCKET_URL = (
-    process.env.NODE_ENV === "production"
-        ? "https://kenneth-music.s3.us-east-2.amazonaws.com"
-        : `${process.env.PUBLIC_URL}/music`
-)
+export const BUCKET_URL = "https://kenneth-music.s3.us-east-2.amazonaws.com"
