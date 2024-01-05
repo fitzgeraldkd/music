2de105b49a899df5f64d7aed922369cca61cc69c
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2024-01-05 18:21:04 -0500

    Update cursor when mousing over canvas

--- src/components/Visualizer.styles.ts
+++ src/components/Visualizer.styles.ts
@@ -1,6 +1,7 @@
 import { type CSSObject } from "@mui/material/styles"
 
 export const VisualizerBoxStyles: CSSObject = {
+    cursor: "all-scroll",
     position: "absolute",
     top: 0,
     left: 0,
